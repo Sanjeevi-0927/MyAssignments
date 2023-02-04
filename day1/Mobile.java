470e33942b6f651989e9b4c523a761d1e4354a86
Sanjeevi-0927 <124467969+Sanjeevi-0927@users.noreply.github.com>
2023-02-05 03:05:33 +0530

    Add files via upload
    day 1

---
+++ day1/Mobile.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Mobile {
+	public static void main(String[] args) {
+		String mobileBrandName="OppoReno 8";
+		System.out.println(mobileBrandName);
+		char mobileLogo='^';
+		System.out.println(mobileLogo);
+		short noOfMobilePiece=1;
+		System.out.println(noOfMobilePiece);
+		int modelNumber=67892648;
+		System.out.println(modelNumber);
+		long mobileImeiNumber=7826387246929829074L;
+		System.out.println(mobileImeiNumber);
+		float mobilePrice= 29500.90F;
+		System.out.println(mobilePrice);
+		boolean isDamaged=false;
+		System.out.println(isDamaged);
+	}
+
+}
---
+++ day1/TwoWheeler.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class TwoWheeler {
+ 
+public static void main(String[] args) {
+	int noOfWheels=1;
+	System.out.println(noOfWheels);
+	short noOfMirrors=2;
+	System.out.println(noOfMirrors);
+	long chassisNumber=1234567812345678L;
+	System.out.println(chassisNumber);
+	boolean isPunctured=true;
+	System.out.println(isPunctured);
+	String bikeName="scooty";
+	System.out.println(bikeName);
+	double runningKM=9908.284;
+	System.out.println(runningKM);
+	
+	
+	
+	
+	
+	
+}
+}
